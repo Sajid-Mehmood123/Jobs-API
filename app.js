022f6f861842bbad4137a6560d6c8c719e8b4006
Sajid-Mehmood123 <mssajidali45@gmail.com>
2024-07-12 22:23:37 +0500

    routes and database

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const authRouter = require("./routes/auth.routes");
+const jobsRouter = require("./routes/jobs.routes");
+
 // error handler
 const notFoundMiddleware = require("./middlewares/not-found");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
+const connectDB = require("./db/connection");
 
 app.use(express.json());
 
 // routes
-app.get("/", (req, res) => {
-  res.send("Job api");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +25,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
