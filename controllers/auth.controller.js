465b4856c2877c0b5b0a9258afa4704dfaebc208
Sajid-Mehmood123 <mssajidali45@gmail.com>
2024-07-13 02:46:29 +0500

    login controller setup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const User = require("../models/user.model");
 const { StatusCodes } = require("http-status-codes");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -10,7 +11,28 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  const token = user.createJSONToken();
+
+  res
+    .status(StatusCodes.OK)
+    .json({ user: { msg: "login successfully", name: user.name }, token });
 };
 
 module.exports = { register, login };
--- models/user.model.js
+++ models/user.model.js
@@ -42,4 +42,9 @@ userSchema.methods.createJSONToken = function () {
   );
 };
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model("User", userSchema);
