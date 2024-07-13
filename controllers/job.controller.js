0c80e6d10e64c0da592248c337c523e87be78abb
Sajid-Mehmood123 <mssajidali45@gmail.com>
2024-07-13 17:51:13 +0500

    job controllers

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -1,12 +1,22 @@
+const { NotFoundError, BadRequestError } = require("../errors");
 const Job = require("../models/jobs.model");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const job = await Job.find({ createdBy: req.user.userId });
+  res.status(StatusCodes.OK).json({ total: job.length, job });
 };
 
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`no job found with this id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
@@ -14,10 +24,36 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("please provide company and position");
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`no job found with thid id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`no job found with thid id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json("job deleted");
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
